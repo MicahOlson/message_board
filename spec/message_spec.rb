4dfb740e83f976fe888b05c0a66224eccda5fe27
Micah L. Olson <76067607+MicahOlson@users.noreply.github.com>
2021-03-04 18:17:39 -0800

    add specs setup to clear messages

--- spec/message_spec.rb
+++ spec/message_spec.rb
@@ -2,16 +2,9 @@ require 'rspec'
 require 'message'
 
 describe '#Message' do
-  
-  describe('.clear') do
-    it("clears all messages") do
-      message = Message.new({:user_post => "Hello humans!", :user => "Mr. Robot", :post_id => nil})
-      message.save()
-      message2 = Message.new({:user_post => "Hello to you too fellow human", :user => "Bender", :post_id => nil})
-      message2.save()
-      Message.clear()
-      expect(Message.all).to(eq([]))
-    end
+
+  before(:each) do
+    Message.clear
   end
 
   describe('#initialize') do
@@ -38,10 +31,21 @@ describe '#Message' do
   describe('#save') do
     it('saves a messsage') do
       message = Message.new({:user_post => "Hello humans!", :user => "Mr. Robot", :post_id => nil})
-      message.save()
+      message.save
       message2 = Message.new({:user_post => "Hello to you too fellow human", :user => "Bender", :post_id => nil})
-      message2.save()
+      message2.save
       expect(Message.all()).to(eq([message, message2]))
+    end
+  end
+
+  describe('.clear') do
+    it("clears all messages") do
+      message = Message.new({:user_post => "Hello humans!", :user => "Mr. Robot", :post_id => nil})
+      message.save
+      message2 = Message.new({:user_post => "Hello to you too fellow human", :user => "Bender", :post_id => nil})
+      message2.save
+      Message.clear
+      expect(Message.all).to(eq([]))
     end
   end
 end
